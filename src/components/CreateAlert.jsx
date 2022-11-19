b2926ac85711d9669cb56300da5934e57c29d85e
Ashley Jiang <ashleyjiang4@gmail.com>
2022-11-19 14:43:19 +0100

    CreateAlert type and timeframe inputs created

--- src/components/CreateAlert.jsx
+++ src/components/CreateAlert.jsx
@@ -1,22 +1,39 @@
 import { useState } from 'react';
 import createAppointment from '../libs/createAppointment';
+import DatePicker from "react-datepicker";
 
-import '../scss/CreatePage.scss';
+import "react-datepicker/dist/react-datepicker.css";
+
+import '../scss/CreateAlert.scss';
+
+const types = [
+    {
+        id: 1,
+        name: "Abholung des Aufenthaltstitels"
+    }
+]
 
 export default function CreateAlert(props) {
     const handleClose = () => props.setShow(false);
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
+    const [type, setType] = useState("");
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(null)
     const [message, setMessage] = useState("");
+
+    const handleTimeFrame = (dates) => {
+        const [start, end] = dates;
+        setStartDate(start);
+        setEndDate(end);
+      };
 
     async function submitHandler(e) {
         e.preventDefault();
-        const request = await createAppointment(title, description);
+        const request = await createAppointment(type, startDate.toISOString(), endDate.toISOString());
         if (request.success) {
             // add universe to current state
-            
             handleClose();
         } else {
+            console.log('request', type, startDate.toISOString(), endDate.toISOString())
             setMessage(request.result);
         }
     }
@@ -24,30 +41,33 @@ export default function CreateAlert(props) {
     return <div className="createPageModal">
         <div className="createPageContainer">
             <h2>
-                New Appointment
+                New Alert
                 <button onClick={handleClose}>
                     X
                 </button>
             </h2>
             <form onSubmit={submitHandler}>
-                <div>
-                    <label htmlFor="title">Title:</label>
-                    <input
-                        type="text" id="title" placeholder="Title" required maxLength={60}
-                        onChange={e => setTitle(e.target.value)}
-                    />
+                <div className='type-container'>
+                    <label htmlFor="title">Type:</label>
+                    <select name="type" id="type-select" onChange={(e) => setType(e.target.value)}>
+                        {types.map(obj => (
+                            <option value={obj.id}>{obj.name}</option>
+                        ))}
+                    </select>
                 </div>
-                <div>
-                    <div>
-                        <label  htmlFor="description">Description:</label>
-                    </div>
-                    <textarea 
-                        rows={10} cols={60} id="description" placeholder="Add a description..." maxLength={9000}
-                        onChange={e => setDescription(e.target.value)} className="description"
+                <div className='time-frame-container'>
+                    <label  htmlFor="timeFrame">Time frame:</label>
+                    <DatePicker
+                        selected={startDate}
+                        onChange={handleTimeFrame}
+                        startDate={startDate}
+                        endDate={endDate}
+                        selectsRange
+                        inline
                     />
                 </div>
                 <button type="submit">
-                    Create Appointment
+                    Create Alert
                 </button>
                 <div>{`${message}`}</div>
             </form>
